4699d17adb7adfe1a19db0cc7e27ef7eb04f30ea
Andres <gomez_andresf@hotmail.com>
2019-07-15 09:55:22 -0500

    Avanzamos en las clases

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    p Nombre
+    input(v-model="name")
+    br
+    br
+    p Apellidos
+    input(v-model="lastname")
+    br
+    p Nombre completo: {{ fullName }}
+    br
+    br
+    p Fecha de nacimiento
+    input(v-model="birthday", placeholder="aaaa-mm-dd")
+    br
+    p Edad: {{ Age }}
+    br
+    br
+    p Tarjeta de credito
+    input(v-model="creditcard")
+    button(v-on:click = "formattedCard") Formato
+    br
+    p {{ creditFormated }}
+    
+
 </template>
 
 <script>
@@ -9,12 +30,74 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola Mundo!'
+      msg: 'Hola Mundo!',
+      name: '',
+      lastname: '',
+      birthday: '',
+      creditcard: '',
+      creditFormated: ''
+    }
+  },
+  computed: {
+    fullName(){
+      return  `${this.name} ${this.lastname}`
+    },
+    Age(){
+      const moment = require('moment')
+      if(this.birthday.length === 10){
+        let fecha = moment(this.birthday)
+        let hoy = moment()
+        return `${hoy.diff(fecha, 'years')} años.`
+      }else{
+        return ''
+      }
+    }
+  },
+  watch:{
+
+  },
+  methods:{
+    formattedCard(){
+      let dividido = this.creditcard.match(/.{1,4}(.$)?/g)
+      this.creditFormated = dividido.join(' ')
     }
   }
 }
+/**
+ * Los watchers (watch) sirven para desencadenar eventos cuando se está haciendo algo en el dom,
+ * Algo así como ir buscando algo conforme se escribe en un input
+ */
+
 </script>
 
 <style lang="scss">
   @import './scss/main.scss';
+  #app {
+    margin-top: 5%;
+    margin-left: 5%;
+    max-width: 420px;
+  }
+  h1 {
+    color: #A3ECF3;
+    font-size: 20px;
+    font-weight: bolder;
+    text-align: center
+  }
+  img {
+    margin:10px auto;
+		display:block;
+  }
+  .label{
+    color:#FF9300;
+    font-size: 11px;
+  }
+  .input{
+    border: none;
+    border-bottom: 1px solid #FF9300;
+    box-shadow: none !important;
+    color:#212121;
+  }
+  p{
+    margin: 15px 15px;
+  }
 </style>
---
+++ src/App_form_login.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+  #app
+    .columns
+      .column
+      .column.is-three-fifths
+        img(src="./assets/logo.jpg")
+        h1 Registro Estación Platzi
+        br
+        .field
+          label.label Nombre
+            .control
+              input.input(type="text", name='nombre', placeholder="Nombre")
+        .field
+          label.label Apellidos
+            .control
+              input.input(type="text", name='apellidos', placeholder="Apellidos")
+        .field
+          label.label Cargo
+            .control
+              input.input(type="text", name='cargo', placeholder="Cargo")
+        .field.is-grouped.is-grouped-right
+          .control
+            button.button.is-primary Enviar
+      .column
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hola Mundo!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss';
+  #app {
+    margin-top: 5%;
+    margin-left: 5%;
+    max-width: 420px;
+  }
+  h1 {
+    color: #A3ECF3;
+    font-size: 20px;
+    font-weight: bolder;
+    text-align: center
+  }
+  img {
+    margin:10px auto;
+		display:block;
+  }
+  .label{
+    color:#FF9300;
+    font-size: 11px;
+  }
+  .input{
+    border: none;
+    border-bottom: 1px solid #FF9300;
+    box-shadow: none !important;
+    color:#212121;
+  }
+</style>
